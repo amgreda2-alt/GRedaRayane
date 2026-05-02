a045fd25efeda483991aad6647aa5fd23c624032
oypfn <soyrayenach@gmail.com>
2026-05-02 17:24:07 +0200

    rayane l7way  commit

--- src/main/java/com/mycompany/proredarayane/ProRedaRayane.java
+++ src/main/java/com/mycompany/proredarayane/ProRedaRayane.java
@@ -11,7 +11,7 @@ public class ProRedaRayane {
 
     public static void main(String[] args) {
         System.out.println("Primer cambio en el repositorio local");
-        System.out.println("Segundo cambio en el repositorio local");
+        System.out.println("Cambio Rayane l7way 2");
         System.out.println("Tercer cambio");
         System.out.println("Quarto cambio");
         System.out.println("Cambio usuarioli 7wak 2");
